7fbced054bd9bef24f70cdd63d2628df32a6418f
ShannonPaige <spaige@gmail.com>
2016-03-21 22:18:10 -0600

    WIP: creating new Idea object not adding timestamps

--- spec/controllers/api/v1/ideas_controller_spec.rb
+++ spec/controllers/api/v1/ideas_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe Api::V1::IdeasController, type: :controller do
 
-  let!(:idea1) { create(:idea) }
-  let!(:idea2) { create(:idea) }
+  let!(:idea1) { Idea.create(title: "Awesome Idea", body: "Awesome Description") }
+  let!(:idea2) { Idea.create(title: "Awesome Idea 2", body: "Awesome Description 2") }
 
   describe "GET #index" do
     it "returns http success" do
+      binding.pry
       get :index, format: :json
       expect(response).to have_http_status(:success)
     end
@@ -32,9 +33,27 @@ describe Api::V1::IdeasController, type: :controller do
       assert_equal idea2.title, object2["title"]
       assert_equal idea2.body, object2["body"]
       assert_equal idea2.quality, object2["quality"]
+    end
+  end
 
+  describe "POST #create" do
+    it "returns http success" do
+      get :create, format: :json, title: 'New Title', body: 'New Body'
+      expect(response).to have_http_status(:success)
     end
 
+    it "returns one object" do
+      get :create, format: :json, title: 'New Title', body: 'New Body'
+      assert_kind_of Hash, json_response
+    end
+
+    it "returns the correct object" do
+      get :create, format: :json, title: 'New Title', body: 'New Body'
+
+      assert_equal idea1.title, json_response["title"]
+      assert_equal idea1.body, json_response["body"]
+      assert_equal idea1.quality, json_response["quality"]
+    end
   end
 
 end
