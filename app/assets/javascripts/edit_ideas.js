4e19eaf8ec867319351b4486d5dbd0adae8d24ac
ShannonPaige <spaige@gmail.com>
2016-03-23 12:20:41 -0600

    Implement inline editing

--- app/assets/javascripts/edit_ideas.js
+++ app/assets/javascripts/edit_ideas.js
@@ -13,9 +13,9 @@ function makeEditable(id){
 
 function saveIdeaButton() {
   $('#all-ideas').on("click", ".save-btn", function(){
-    var id = this.id.replace('save-btn-', '');
+    var id = this.id.replace('edit-btn-', '');
     var idea_params = { title: $('.editing h3').text(), body: $('.editing p').text() }
-    updateIdea(idea_params);
+    updateIdea(id, idea_params);
   })
 }
 
@@ -26,7 +26,7 @@ function updateIdea(id, idea_params){
     data: idea_params,
     success: function(response){
       console.log('updateIdea SUCCESS', response)
-      saveIdea(id)
+      saveIdea()
     }, error: function(xhr){
       console.log('updateIdea FAIL', xhr)
     }
@@ -34,7 +34,8 @@ function updateIdea(id, idea_params){
 }
 
 function saveIdea(){
-  $('#idea-'+id+' .editable').removeClass('editing')
-  document.querySelectorAll('#idea-'+id+' .editable')[0].contentEditable = false;
-  $('#idea-'+id+' .edit-btn').text('Edit Idea').removeClass('save-btn').addClass('edit-btn')
+  debugger;
+  $('.editable').removeClass('editing')
+  document.querySelectorAll('.editable')[0].contentEditable = false;
+  $('.save-btn').text('Edit Idea').removeClass('save-btn').addClass('edit-btn')
 }
